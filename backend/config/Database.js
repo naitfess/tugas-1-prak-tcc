c9dae247ffee46240b2e329ab1ada0090a7bccad
Seftian Alung <142821779+naitfess@users.noreply.github.com>
2025-04-20 20:09:49 +0700

    Update Database.js

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db-alung", "root", "slemanmagelang", {
-  host: "34.66.179.32",
+const db = new Sequelize("db-alung", "root", "", {
+  host: "34.170.57.16",
   dialect: "mysql",
 });
 
