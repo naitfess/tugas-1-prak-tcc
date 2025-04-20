ebe9d08171a91e9cfdaedead252e767f7b5f557f
naitfess <naitfess@gmail.com>
2025-04-20 19:52:40 +0700

    update db

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db-alung", "root", "", {
-  host: "34.170.57.16",
-  // host: "localhost",
+const db = new Sequelize("db-alung", "root", "slemanmagelang", {
+  host: "34.66.179.32",
   dialect: "mysql",
 });
 
